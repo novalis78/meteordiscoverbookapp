6a69df2e2e63ae244ced22ccbf24f3f9e470b93d
novalis78 <novalis78@crunchbang>
2013-12-14 23:34:58 -0500

    first commit

---
+++ collections/comments.js
@@ -0,0 +1,34 @@
+Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+
+	comment: function(commentAttributes){
+
+		var user = Meteor.user();
+
+		var post = Posts.findOne(commentAttributes.postId);
+
+		if(!user)
+			throw new Meteor.Error(401, "You need to login");
+
+		if(!commentAttributes.body)
+			throw new Meteor.Error(422, "Please write something");
+
+		if(!post)
+			throw new Meteor.Error(422, "You must comment on a post");
+
+		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+
+		});
+
+		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+
+		comment._id = Comments.insert(comment);
+	    // now create a notification, informing the user that there's been a comment
+	    createCommentNotification(comment);
+	    return comment._id;
+	}
+})
